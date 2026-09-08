41f765a099e169ff2251387984306c1d974671fa
Jurakluka18 <jurakluka18@gmail.com>
2026-09-08 09:56:19 +0200

    Route /admin to admin dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App";
+import Admin from "./Admin";
+
+const isAdminPath = window.location.pathname === "/admin" || window.location.pathname.startsWith("/admin/");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {isAdminPath ? <Admin /> : <App />}
   </StrictMode>
 );
